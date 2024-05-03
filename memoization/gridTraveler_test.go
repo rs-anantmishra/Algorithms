eb636e268f6b538bb5e915e7093b7b166bbdcb40
Anant Mishra <anantmishra@outlook.com>
2024-05-03 12:46:09 +0530

    benchmarking for grid traveller

---
+++ memoization/gridTraveler_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"testing"
+)
+
+func BenchmarkGridTraveler(b *testing.B) {
+	var args []string
+	args = append(args, "nil")
+	args = append(args, "grid")
+	args = append(args, "18")
+	args = append(args, "18")
+	args = append(args, "m")
+
+	for i := 0; i < b.N; i++ {
+		callGridTraveler(args)
+	}
+
+}
