1a5701ea5677ba793a1c4c5d07f7233c5b74235a
Anant Mishra <rs.anantmishra@gmail.com>
2024-05-08 10:29:51 +0530

    bestSum

---
+++ memoization/canConstruct.go
@@ -0,0 +1,12 @@
+package main
+
+func CallCanConstruct(args []string) {
+
+	targetWord := "abcdef"
+	wordbank := []string{"ab", "abc", "cd", "def", "abcd"}
+	canConstruct(targetWord, wordbank)
+}
+
+func canConstruct(targetWord string, wordbank []string) {
+
+}
