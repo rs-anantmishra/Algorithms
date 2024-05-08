366619b29d4cfabfa24a8e115b2831b36603a15e
Anant Mishra <rs.anantmishra@gmail.com>
2024-05-08 21:44:20 +0530

    plagiarism complete

--- practice/plagiarism.go
+++ practice/plagiarism.go
@@ -2,6 +2,11 @@
 	Find the longest common subsequence, given
 	string1: i have this book i like to read
 	string2: she has this book i like to watch
+
+	As such this is not a dynamic programming problem
+	since we are trying to find longest consecutive common
+	subsequence - since its consecutive there are no
+	repeating problems.
 */
 
 package main
@@ -12,48 +17,48 @@ import (
 )
 
 func main() {
+	space := " "
+	resultSubsequence := []string{}
+	result := ""
 
 	text := "i have this book i like to read"
 	wordbank := "she has this book i like to watch"
 
-	result := longestSubsequence(strings.Split(text, " "), strings.Split(wordbank, " "))
-	fmt.Println(resultSubsequence, result)
-}
+	for len(text) > 0 {
 
-var subsequence string
-var resultSubsequence string
+		if result == "" {
+			text, _ = strings.CutPrefix(text, strings.Split(text, " ")[0])
+		} else {
+			text, _ = strings.CutPrefix(text, result)
+		}
+		text = strings.Trim(text, space)
+		result = longestSubsequence(text, strings.Split(wordbank, " "))
 
-func longestSubsequence(text []string, wordbank []string) string {
-
-	space := " "
-	var result string
-	//base case
-	if len(text) == 0 {
-		return result
+		resultSubsequence = append(resultSubsequence, result)
 	}
 
-	for i, value := range text {
-		for k, word := range wordbank {
-			if value == word {
-				subsequence = word + space + subsequence
-				if len(resultSubsequence) == 0 || len(resultSubsequence) < len(subsequence) {
-					resultSubsequence = subsequence
-				}
-				if longestSubsequence(text[i:], wordbank[k:]) == "" {
-					return result
-				}
+	fmt.Println(resultSubsequence)
+}
+
+func longestSubsequence(text string, sequence []string) string {
+
+	var subsequence string
+	space := " "
+	//base case
+	if len(text) == 0 {
+		return subsequence
+	}
+
+	for i, word := range sequence {
+		if strings.Index(text, word) == 0 {
+
+			subsequence = subsequence + word
+			suffix, _ := strings.CutPrefix(text, (word + space))
+			if longestSubsequence(suffix, sequence[i:]) == "" {
+				return subsequence
 			}
 		}
 	}
 
-	return result
+	return subsequence
 }
-
-//<space> should be ignored to be used as a suffix
-//(i have) this book i like to read
-//for the same suffix and within a same contiguous sequence
-////////subtract from the wordbank if a word is used.
-
-//string1: (i have) this book i like to read
-//string2: she has this book i like to watch
-//this is a test sample from an interview
