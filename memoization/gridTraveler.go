d15019d1fb76ac04af0512ccfac2d4414f637b30
Anant Mishra <rs.anantmishra@gmail.com>
2024-05-08 10:29:50 +0530

    Grid Traveller Memoized

--- memoization/gridTraveler.go
+++ memoization/gridTraveler.go
@@ -1,5 +1,38 @@
 package main
 
-func traveler() {
+type Grid struct {
+	Row int
+	Col int
+}
 
+func GridTravelerMemo(g Grid, m map[Grid]int) int {
+	//fast-return case
+	_, ok := m[g]
+	if ok {
+		return m[g]
+	}
+	//base cases
+	if g.Row == 0 || g.Col == 0 {
+		return 0
+	}
+	if g.Row == 1 || g.Col == 1 {
+		return 1
+	}
+
+	m[g] = GridTravelerMemo(Grid{g.Row - 1, g.Col}, m) + GridTravelerMemo(Grid{g.Row, g.Col - 1}, m)
+	return m[g]
+}
+
+func GridTraveler(m int, n int) int {
+
+	//base cases
+	if m == 0 || n == 0 {
+		return 0
+	}
+
+	if m == 1 || n == 1 {
+		return 1
+	}
+
+	return GridTraveler(m-1, n) + GridTraveler(m, n-1)
 }
--- memoization/main.go
+++ memoization/main.go
@@ -8,12 +8,55 @@ import (
 
 func main() {
 
-	n, _ := strconv.Atoi(os.Args[1])
-	var computation string
-	if len(os.Args) == 3 {
-		computation = os.Args[2]
+	if os.Args[1] == "fib" {
+		//os.Args = append(os.Args, "fib")
+		//os.Args = append(os.Args, 100)
+		//os.Args = append(os.Args, "m")
+		callFib(os.Args)
 	}
 
+	if os.Args[1] == "grid" {
+		// os.Args = append(os.Args, "grid")
+		// os.Args = append(os.Args, "18")
+		// os.Args = append(os.Args, "18")
+		// os.Args = append(os.Args, "m")
+		callGridTraveler(os.Args)
+	}
+
+}
+
+func callGridTraveler(args []string) {
+
+	var computation string
+	//In how many ways can you travel to the goal of a grid of dim=m*n
+	//only move down or right. //start: top-left //end: bottom-right
+	if len(args) == 5 {
+		computation = args[4]
+	}
+
+	m, _ := strconv.Atoi(args[2])
+	n, _ := strconv.Atoi(args[3])
+
+	var g Grid
+	g.Row = m
+	g.Col = n
+	var mapster = make(map[Grid]int)
+
+	switch {
+	case computation == "memo" || computation == "Memo" || computation == "m":
+		fmt.Println(GridTravelerMemo(g, mapster))
+	default:
+		fmt.Println(GridTraveler(m, n))
+	}
+
+}
+
+func callFib(args []string) {
+	var computation string
+	n, _ := strconv.Atoi(args[2])
+	if len(args) == 4 {
+		computation = args[3]
+	}
 	var m = make(map[int]int)
 
 	switch {
@@ -24,5 +67,4 @@ func main() {
 	default:
 		fmt.Println("memoized", fibMemo(n, m))
 	}
-
 }
