8d6e330e129b30ac5816a5d674465637e980b4e1
Anant Mishra <rs.anantmishra@gmail.com>
2024-05-08 10:29:50 +0530

    memoized fibonacci in go

--- memoization/fib.go
+++ memoization/fib.go
@@ -7,8 +7,24 @@ import (
 )
 
 func main() {
+
 	n, _ := strconv.Atoi(os.Args[1])
-	fmt.Println(fib(n))
+	var computation string
+	if len(os.Args) == 3 {
+		computation = os.Args[2]
+	}
+
+	var m = make(map[int]int)
+
+	switch {
+	case computation == "memo" || computation == "Memo" || computation == "m":
+		fmt.Println("memoized", fibMemo(n, m))
+	case n < 40:
+		fmt.Println("standard", fib(n))
+	default:
+		fmt.Println("memoized", fibMemo(n, m))
+	}
+
 }
 
 func fib(n int) int {
@@ -17,3 +33,22 @@ func fib(n int) int {
 	}
 	return fib(n-2) + fib(n-1)
 }
+
+func fibMemo(n int, m map[int]int) int {
+
+	_, ok := m[n]
+	if ok {
+		return m[n]
+	}
+	if n <= 2 {
+		return 1
+	}
+
+	m[n] = fibMemo(n-2, m) + fibMemo(n-1, m)
+	return m[n]
+
+}
+
+// considering fib's starting position 0 with value 0
+// 0	1	1	2	3	5	8	13  21
+// 0	1	2	3	4	5	6	7	8
